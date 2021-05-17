e7a7f2986fcf99f1375544bd108186e3919af66c
DrecoJohnson <70784452+DrecoJohnson@users.noreply.github.com>
2021-05-17 14:59:17 -0400

    Add files via upload

---
+++ Passenger_Counter/index.js
@@ -0,0 +1,19 @@
+document.getElementById("count-el").innerText = 5
+
+let count = 0
+
+console.log(count)
+
+
+// 1. Create a variable, myAge, and set its value to your age
+
+// 2. Log the myAge variable to the console
+
+//  let myAge = 25
+// console.log(myAge)
+
+//let myAge = 25
+//let humanDogRatio = 7
+//let myDogAge = myAge * humanDogRatio
+
+//console.log(myDogAge)
